7f30572b08b7a0a111ce43f3a7258c96ec770993
Michael Mena <michael.mena2408@gmail.com>
2025-06-14 22:20:58 -0300

    first commit

---
+++ src/main/java/FIUBA/CineXplore/CineXploreApplication.java
@@ -0,0 +1,13 @@
+package FIUBA.CineXplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineXploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineXploreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FIUBA/CineXplore/CineXploreApplicationTests.java
@@ -0,0 +1,13 @@
+package FIUBA.CineXplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineXploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
